70f2856b9fa5ca3fdf030706c9690e9b22cc4bd1
yinli <13004327958@163.com>
2021-09-23 10:36:16 +0800

    kkk

--- src/main/java/com/atguigu/git/GItTest.java
+++ src/main/java/com/atguigu/git/GItTest.java
@@ -12,6 +12,7 @@ public class GItTest {
         System.out.println("master test!");
         System.out.println("hot-fix test");
         System.out.println("push test");
+        System.out.println("gitee test");
         //zhushi
         //zhushi pull
         //disancikk
